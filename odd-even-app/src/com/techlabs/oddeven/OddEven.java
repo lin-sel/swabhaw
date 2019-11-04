4bc3ef6cebe84259fe44e1fbe52d9ea81b188d32
lin-sel <ccnsy86@gmail.com>
2019-11-04 15:45:10 +0530

    Design Modified

--- odd-even-app/src/com/techlabs/oddeven/OddEven.java
+++ odd-even-app/src/com/techlabs/oddeven/OddEven.java
@@ -1,15 +1,48 @@
 package com.techlabs.oddeven;
 
+import java.util.ArrayList;
+
 public class OddEven {
-	public void printOddEven(int minrange, int maxrange) {
-		System.out.println("Even"+" "+"Odd");
+	
+	// Print Even Number.
+	public ArrayList<Integer> getEvenNumber(int minrange, int maxrange) {
+		System.out.println();
+		System.out.println("Even");
+		int num;
+		ArrayList<Integer> evenumber = new ArrayList<>();
 		for(int min = minrange; min <= maxrange; min+=2) {
-			if(min%2 == 0) {
-				System.out.println(min+"  "+min+1);
-			}
-			else {
-				System.out.println(min+1+"  "+min);
-			}
+			num = checkNumber(min,0);
+			if(num <= maxrange)
+				evenumber.add(num);
 		}
+		
+		return evenumber;
 	}
+	
+	// Print Odd Number.
+	public ArrayList<Integer> getOddNumber(int minrange, int maxrange) {
+		System.out.println();
+		System.out.println("Odd");
+		int num;
+		ArrayList<Integer> oddnumber = new ArrayList<>();
+		for(int min = minrange; min <= maxrange; min+=2) {
+			num = checkNumber(min,1);
+			if(num <= maxrange)
+				oddnumber.add(num);
+		}
+		return oddnumber;
+	}
+	
+	// Check Number Even or Odd.
+	private static int checkNumber(int num,int flag) {
+		if(num%2==0 && flag == 0) {
+			return num;
+		}
+		else if(num%2 != 0 && flag == 1){
+			return num;
+		}
+		
+		return num+1;
+	}
+	
 }
--- odd-even-app/src/com/techlabs/oddeven/test/OddEvenTest.java
+++ odd-even-app/src/com/techlabs/oddeven/test/OddEvenTest.java
@@ -1,12 +1,25 @@
 package com.techlabs.oddeven.test;
 
+import java.util.ArrayList;
+
 import com.techlabs.oddeven.OddEven;
 
 public class OddEvenTest {
 
 	public static void main(String[] args) {
+		ArrayList<Integer> number = new ArrayList<Integer>();
 		OddEven oe = new OddEven();
-		oe.printOddEven(1, 100);
+		number = oe.getEvenNumber(51, 100);
+		
+		number.forEach((n) -> printNumber(n));
+		number = oe.getOddNumber(50, 100);
+		System.out.println();
+		number.forEach((n) -> printNumber(n));
+	}
+
+
+	public static void printNumber(int num) {
+		System.out.print(num+" ");
 	}
 
 }
