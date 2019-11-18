1a05f39fb3952616f37998ef1d28825b18064536
lin-sel <ccnsy86@gmail.com>
2019-11-18 17:23:50 +0530

    Exception

---
+++ Basic-app/exception-app/src/com/techlabs/exception/Exception.java
@@ -0,0 +1,13 @@
+package com.techlabs.exception;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		int a = args[0];
+		int b = args[1];
+		int c = args[2];
+		System.out.println(c);
+		System.out.println("End of the Program");
+	}
+
+}
