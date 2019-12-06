1eb03356913fbc64a82bebd8fd70ed9837ccad6f
lin-sel <ccnsy86@gmail.com>
2019-12-06 22:47:05 +0530

    Basic Blue Ball Game

---
+++ JavaScript/js-app/blueball/blueball.js
@@ -0,0 +1,64 @@
+(function () {
+
+    const buttonsec = document.getElementById("buttonsec");
+    const displaynotification = document.getElementById("display");
+    var detail = document.getElementById("details");
+    const maxbutton = 50;
+    var selectedbutton;
+    var attempt = 5;
+
+    document.getElementById("startgame").addEventListener("click", () => {
+        document.getElementById("rulessection").style.display = "none";
+        document.getElementById("main").style.display = "block";
+        console.log(selectedbutton);
+    });
+
+
+    window.onload = function () {
+        for (var init = 1; init <= maxbutton; init++) {
+            var button = document.createElement("button");
+            button.innerHTML = init.toString();
+            button.addEventListener("click", checkbutton);
+            buttonsec.append(button);
+        }
+        randomButtonSelect();
+        checkAttempt();
+    }
+
+
+    function randomButtonSelect() {
+        selectedbutton = Math.floor(Math.random() * 100) % maxbutton;
+        if (selectedbutton == 0) {
+            randomButtonSelect();
+        }
+    }
+
+    function checkbutton(e) {
+        attempt -= 1;
+        checkAttempt();
+        if (e.target.innerHTML == selectedbutton) {
+            e.target.style.backgroundColor = "blue";
+            alert("You Have Win the Game.");
+            setTimeout(() => {
+                location.reload();
+            }, 5000);
+            return;
+        }
+        else if (Number(e.target.innerHTML) > selectedbutton) {
+            e.target.style.backgroundColor = "red";
+            return;
+        }
+        e.target.style.backgroundColor = "green";
+    }
+
+    function checkAttempt() {
+        if (attempt > 0) {
+            detail.innerHTML = "Total Attempt left: " + attempt;
+            return;
+        }
+        detail.innerHTML = "Total Attempt left: " + attempt + "<br/> Lose the Game.";
+        setTimeout(() => {
+            location.reload();
+        }, 3000);
+    }
+}());
