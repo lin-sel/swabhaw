4380d06e29963196697a1b0fa166d9ffc245b223
lin-sel <ccnsy86@gmail.com>
2019-12-24 18:21:17 +0530

    Add Pipes to App Module

--- angular/welcome-app/src/app/app.module.ts
+++ angular/welcome-app/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Snake } from './pipes/snake.pipe';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { StudentComponent } from './student/student.component';
+import { BlueballComponent } from './blueball/blueball.component';
+import { TwoWayComponent } from './two-way/two-way.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     StudentComponent,
+    BlueballComponent,
+    TwoWayComponent,
+    Snake
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [StudentComponent]
+  bootstrap: [TwoWayComponent]
 })
 export class AppModule { }
