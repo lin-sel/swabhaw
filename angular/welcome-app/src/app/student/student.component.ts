7b321311641d19c3bcd168ea4f0fedc45bfe0529
lin-sel <ccnsy86@gmail.com>
2019-12-24 18:22:03 +0530

    Modify student List and use *ngFor

--- angular/welcome-app/src/app/student/student.component.ts
+++ angular/welcome-app/src/app/student/student.component.ts
@@ -6,29 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  student: IStudent;
+  student: IStudent[];
+  manyStudent: IStudent[];
+  imgmaxWidth: number = 50;
+  imgmaxHeight: number = 50
   constructor() {
     console.log('Inside Constructor');
   }
 
   ngOnInit() {
     console.log("Inside ngOnInit");
-    this.student = {
+    this.student = [{
       id: 201,
       name: "Nilesh",
       cgpa: 2.6,
       profile: "assets/img/img.png"
-    };
+    }, {
+      id: 301,
+      name: "ABC",
+      cgpa: 7.1,
+      profile: "assets/img/img.png"
+    }, {
+      id: 401,
+      name: "XYZ",
+      cgpa: 6.1,
+      profile: "assets/img/img.png"
+    }, {
+      id: 311,
+      name: "PQR",
+      cgpa: 9.1,
+      profile: "assets/img/img.png"
+    }, {
+      id: 381,
+      name: "QRT",
+      cgpa: 5.1,
+      profile: "assets/img/img.png"
+    }];
 
     this.change();
   }
 
   change() {
     setTimeout(() => {
-      this.student.cgpa = 8.2
+      this.student[0].cgpa = 8.2
     }, 2000);
   }
 
+  loadData() {
+    console.log('Event Fire');
+    this.manyStudent = this.student;
+  }
+
+  changeColor(cgpa) {
+    if (cgpa <= 5.1) {
+      return 'red';
+    }
+    else if (cgpa > 5.1 && cgpa < 8.1) {
+      return 'yellow';
+    }
+    return 'green';
+  }
 
 }
 
